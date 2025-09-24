0bcb3610d6f0a9a39bdf9f46c8857f1c02ffdf1c
Yugesh Maharjan <yugeshmaharjan16@gmail.com>
2025-09-24 18:54:40 +0545

    Version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Header/>
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <>
+        <div className="headerContainer">
+            <img src="/assets/profile.jpg" alt="Profile Picture" />
+
+            <h3>
+                beatae voluptatibus officiis quae similique eaque hic v
+            </h3>
+            <p>
+                beatae voluptatibus officiis quae similique eaque hic vbeatae voluptatibu
+            </p>
+            <div className="resume">
+                My Resume
+            </div>
+        </div>
+
+    </>
+      
+  );
+};
+
+export default Header;
