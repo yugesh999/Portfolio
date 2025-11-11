865e7c7ead73030b3bd57f9428bd2db1cd33aba0
Yugesh Maharjan <yugeshmaharjan16@gmail.com>
2025-11-11 09:59:36 +0545

    Change Skills

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -18,9 +18,9 @@ const AboutMe = () => {
             </div>
             <div className="about-skills">
                 <div className="about-skill"><p>HTML & CSS</p><hr style={{width: "70%"}}/></div>
-                <div className="about-skill"><p>React JS</p><hr style={{width: "50%"}}/></div>
-                <div className="about-skill"><p>JavaScript</p><hr style={{width: "60%"}}/></div>
-                <div className="about-skill"><p>NextJs</p><hr style={{width: "40%"}}/></div>
+                <div className="about-skill"><p>JavaScript</p><hr style={{width: "50%"}}/></div>
+                <div className="about-skill"><p>React Js</p><hr style={{width: "60%"}}/></div>
+                <div className="about-skill"><p>GitHub</p><hr style={{width: "40%"}}/></div>
             </div>
         </div>
       </div>
